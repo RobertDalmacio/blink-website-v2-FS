f09b6a08a0086d80876a48f0ff2bdfaed20a9ffa
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-08-09 16:49:56 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const middlewares = jsonServer.defaults({
 
 const port = process.env.PORT || 4000
 server.use(middlewares)
-server.use(
-    jsonServer.rewriter({
-        '/api/*': '/$1',
-    })
-)
+// server.use(
+//     jsonServer.rewriter({
+//         '/api/*': '/$1',
+//     })
+// )
 
 server.use(router)
 server.listen(port, () => {
